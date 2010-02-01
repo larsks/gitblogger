67ceb167d7af219e39a6fc0ea1060a22f31cc343
Lars Kellogg-Stedman <lars@oddbit.com>
2010-01-31 20:28:46 -0500

    fixed some packaging details -- sdist now includes README.rst

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='gitblogger',
         version='20100130.3',
@@ -9,8 +13,10 @@ setup(name='gitblogger',
             'configdict',
             ],
         description='Publish reStructuredText documents to Blogger from a Git repository.',
+        long_description=read('README.rst'),
         author='Lars Kellogg-Stedman',
         author_email='lars@oddbit.com',
         packages=['gitblogger'],
         scripts=['scripts/gitblogger-post-receive',],
         )
+
