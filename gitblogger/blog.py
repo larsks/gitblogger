88cd734fdb2d367dc0d85e295f1a5102d99d1fc1
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2010-02-03 20:52:21 -0500

    - added docs to gitblogger/blog.py - added wrapper for get_posts() - fixed debug code to use configdict instead   of ConfigParser

--- gitblogger/blog.py
+++ gitblogger/blog.py
@@ -13,23 +13,44 @@ class NoSuchBlogError (BlogError):
     pass
 
 class Blog (object):
+    '''This is a simple class for interacting with a Blogger blog.
+    It uses the Google gdata.blogger.client API to create, remove,
+    and update Blogger posts.'''
 
     def __init__ (self, config):
+        '''``config`` is a configuration dictionary that must provide
+        values for:
+        
+        - *username* -- your Blogger username
+        - *password* -- the corresponding password
+        - *blog* -- the name of your blog
+        '''
+
         self.config = config
         self.log = logging.getLogger('gitblogger.blog')
         self.blog_name = config['blog']
 
+        print 'CONFIG:', self.config
+
+        self.connect()
+        self.find_blog()
+
+    def connect(self):
+        '''Authenticate to Blogger.'''
+
         self.log.info('Connecting to %(blog)s as %(username)s.' %
-                config)
+                self.config)
         self.client = gdata.blogger.client.BloggerClient()
         self.client.client_login(
-                config['username'],
-                config['password'],
+                self.config['username'],
+                self.config['password'],
                 'com.oddbit.gitblogger',
                 service='blogger')
 
-        # Iterate through blogs looking for one with the appropate 
-        # title.
+    def find_blog(self):
+        '''Iterate through blogs looking for one with the title
+        selected in our configuration.'''
+
         feed = self.client.get_blogs()
         for entry in feed.entry:
             if entry.title.text == self.blog_name:
@@ -40,6 +61,14 @@ class Blog (object):
             raise NoSuchBlogError(self.blog_name)
 
     def add_post(self, doc, draft=None):
+        '''Add a post to the blog.  ``doc`` is a document object that
+        provides the following attributes:
+
+        - *title* -- document title
+        - *content* -- document content (HTML)
+        - *docinfo* -- document metadata dictionary
+        '''
+
         if draft is None:
             draft = doc.docinfo.get('draft', 'False') == 'True'
 
@@ -51,10 +80,19 @@ class Blog (object):
                 draft=draft)
 
     def get_post(self, post_id):
+        '''Retrieve a post by post id.  Returns a
+        ``gdata.blogger.data.BlogPost`` object.'''
+
         return self.client.get_feed(
                 self.blog.get_post_link().href + '/%s' % post_id,
                 auth_token=self.client.auth_token,
                 desired_class=gdata.blogger.data.BlogPost)
+
+    def get_posts(self):
+        '''Like gdata.blogger.client.BloggerClient.get_posts(), but
+        blog_id is provided automatically.'''
+
+        return self.client.get_posts(self.blog_id)
         
     def update_post(self, post, doc):
         '''Update title, content, and tags of the given document.  Note 
@@ -74,13 +112,14 @@ class Blog (object):
         return self.client.update(post)
 
     def delete(self, post):
+        '''Delete a post.'''
         return self.client.delete(post)
 
 if __name__ == '__main__':
     import rstdoc
-    from ConfigParser import ConfigParser
-    cf = ConfigParser()
-    cf.read(sys.argv[1])
+    import configdict
+    logging.basicConfig(level=logging.DEBUG)
 
-    b = Blog(cf)
+    cf = configdict.ConfigDict(sys.argv[1])
+    b = Blog(cf['gitblogger'])
 
